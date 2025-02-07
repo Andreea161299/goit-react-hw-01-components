6e3221494790b343750ccb3da8832cf458bb276f
Andreea161299 <andreea.cornea.ca@gmail.com>
2025-02-07 10:14:14 +0200

    solve problems

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id} className={css.item}>
+          <span className={isOnline ? css.online : css.offline}></span>
+          <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+          <p className={css.name}>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
